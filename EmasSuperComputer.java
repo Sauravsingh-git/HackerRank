81247cf66c92debca3dd83464da437b5a9e95e16
Sauravsingh-git <62598355+Sauravsingh-git@users.noreply.github.com>
2020-06-13 22:15:17 +0530

    Ema's SuperComputer

--- EmasSuperComputer.java
+++ EmasSuperComputer.java
@@ -1,115 +1,106 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-    static class Plus {
-        int x;
-        int y;
-        int len;
-        int size;
-        
-        Plus(int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.len = 0;
-            this.size = 1;
-        }
-        
-        Plus(int x, int y, int len, int size) {
-            this.x = x;
-            this.y = y;
-            this.len = len;
-            this.size = size;
-        }
-        
-        void grow() {
-            this.len++;
-            this.size += 4;
-        }
-        
-        boolean overlaps(Plus p2, int r, int c) {
-        
-            if(this.x != p2.x && this.y == p2.y) {
-                if(p2.x >= this.x - this.len && 
-                   p2.x <= this.x + this.len ||
-                   p2.x < this.x - this.len && 
-                   p2.x + p2.len >= this.x - this.len ||
-                   p2.x > this.x + this.len && 
-                   p2.x - p2.len <= this.x + this.len)
-                    return true;                
-            }
-            
-            if(this.y != p2.y && this.x == p2.x) {
-                if(p2.y >= this.y - this.len && 
-                   p2.y <= this.y + this.len ||
-                   p2.y < this.y - this.len && 
-                   p2.y + p2.len >= this.y - this.len ||
-                   p2.y > this.y + this.len && 
-                   p2.y - p2.len <= this.y + this.len)
-                    return true;
-            }
-            
-            boolean[][] grid = new boolean[r][c];
-            
-            for(int i = this.x - this.len; i <= this.x + this.len; i++) 
-                grid[this.y][i] = true;
-            for(int i = this.y - this.len; i <= this.y + this.len; i++)
-                grid[i][this.x] = true;
-            
-            for(int i = p2.x - p2.len; i <= p2.x + p2.len; i++) {
-                if(grid[p2.y][i] == true)
-                    return true;          
-            }
-            for(int i = p2.y - p2.len; i <= p2.y + p2.len; i++) {
-                if(grid[i][p2.x] == true)
-                    return true;             
-            }
-            return false;
-        }
-    } 
-    
-    static void findPlus(int x, int y, int r, int c, String[] grid, ArrayList<Plus> list) {
-        Plus plus = new Plus(x, y);
-        int s = 1;
-        while(y-s >= 0 && grid[y-s].charAt(x) != 'B' &&
-              y+s < r && grid[y+s].charAt(x) != 'B' &&
-              x-s >= 0 && grid[y].charAt(x-s) != 'B' &&
-              x+s < c && grid[y].charAt(x+s) != 'B') {
-            list.add(new Plus(plus.x, plus.y, plus.len, plus.size));
-            plus.grow();
-            s++;
-        }
-        list.add(plus);
-    }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-        sc.nextLine();
-        String[] grid = new String[r];
-        ArrayList<Plus> list = new ArrayList<Plus>();
-        
-        for(int y = 0; y < r; y++) {
-            grid[y] = sc.nextLine();
-        }
-        
-        for(int y = 0; y < r; y++) {
-            for(int x = 0; x < c; x++) {
-            	if(grid[y].charAt(x) != 'B')
-                    findPlus(x, y, r, c, grid, list);
-            }
-        }        
-        
-        int max = 0;
-        for(int i = 0; i < list.size()-1; i++) {
-            Plus p1 = list.get(i);
-            for(int j = i+1; j < list.size(); j++) {
-                Plus p2 = list.get(j);
-                int sum = p1.size * p2.size; 
-                if(sum > max && !p1.overlaps(p2, r, c)) {
-                    max = sum;
-                }  
-            }
-        }
-        System.out.println(max);
-    }
+public class Solution {
+	private static Scanner sc = new Scanner(System.in);
+
+	public static class plus {
+		int x, y, length, size;
+
+		plus(int x, int y) {
+			this.x = x;
+			this.y = y;
+			this.length = 0;
+			this.size = 1;
+		}
+
+		plus(int x, int y, int length, int size) {
+			this.x = x;
+			this.y = y;
+			this.length = length;
+			this.size = size;
+		}
+
+		void grow() {
+			this.length++;
+			this.size += 4;
+		}
+
+		boolean overlap(plus p2, int r, int c) {
+			boolean[][] grid = new boolean[r][c];
+
+			for (int i = this.x - this.length; i <= this.x + this.length; i++) {
+				grid[this.y][i] = true;
+			}
+
+			for (int i = this.y - this.length; i <= this.y + this.length; i++) {
+				grid[i][this.x] = true;
+			}
+
+			for (int i = p2.x - p2.length; i <= p2.x + p2.length; i++) {
+				if (grid[p2.y][i] == true)
+					return true;
+			}
+
+			for (int i = p2.y - p2.length; i <= p2.y + p2.length; i++) {
+				if (grid[i][p2.x] == true)
+					return true;
+			}
+			return false;
+		}
+
+	}
+
+	static void findPlus(int x, int y, int r, int c, String[] grid, ArrayList<plus> list) {
+
+		plus p = new plus(x, y);
+		int s = 1;
+		while (x - s >= 0 && grid[x-s].charAt(y) != 'B' && y - s >= 0 && grid[x].charAt(y-s) != 'B' && x + s < r
+				&& grid[x+s].charAt(y) != 'B' && y + s < c && grid[x].charAt(y+s) != 'B') {
+
+			list.add(p);
+			s++;
+			p.grow();
+		}
+		list.add(p);
+
+	}
+
+	public static void main(String[] args) {
+		String[] s = sc.nextLine().split(" ");
+		int r, c;
+		r = Integer.parseInt(s[0]);
+		c = Integer.parseInt(s[1]);
+
+		ArrayList<plus> list = new ArrayList<>();
+
+		String[] grid = new String[r];
+
+		for (int i = 0; i < r; i++)
+			grid[i] = sc.nextLine();
+
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (grid[i].charAt(j) != 'B')
+					findPlus(i, j, r, c, grid, list);
+			}
+		}
+
+		int maxArea = 0;
+
+		for (int i = 0; i < list.size(); i++) {
+			plus p1 = list.get(i);
+			for (int j = i + 1; j < list.size(); j++) {
+				plus p2 = list.get(j);
+				int area = p1.size * p2.size;
+
+				if (maxArea < area && !p1.overlap(p2, r, c))
+					maxArea = area;
+			}
+		}
+
+		System.out.print(maxArea);
+
+	}
+
 }
